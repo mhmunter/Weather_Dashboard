7b0b47b40392efda7c69cc058e07a5ce7392cee7
M Munter <mark.munter1@gmail.com>
2022-03-16 16:27:21 -0500

    added push functionality

--- assets/js/Weather.js
+++ assets/js/Weather.js
@@ -1,3 +1,11 @@
+// var userInput = document.getElementsByClassName('search');
+// var userInputData = userInput.trim;
+// var fetchButton = document.getElementById('subBut');
+// var nameValue = document.getElementById('first_name');
+var existDiv = document.getElementById("weather_days");
+
+
+
 
 var apiCall = function(){
 fetch("https://api.openweathermap.org/data/2.5/onecall?lat=29.42&lon=-98.49&units=imperial&dt&exclude=hourly&appid=39397c2783cc72f286060624f17eae92")
@@ -7,27 +15,53 @@ fetch("https://api.openweathermap.org/data/2.5/onecall?lat=29.42&lon=-98.49&unit
             .then(response => response.json()) 
             //Now that we have the data, let us see what it looks like in console
             .then(responseData => {
-                console.log(responseData);
+                // console.log(responseData);
                 // Temp   console.log(responseData.daily[0].temp.day);
                 // console.log(responseData.daily[0].wind_speed);
                 // console.log(responseData.daily[0].humidity);
                 // console.log(responseData.daily[0].uvi);
                 // var base = responseData.daily[0]
                 // console.log(base.uvi)
+
+                // listItem.textContent = data[i].html_url;
+
+
                 for (i = 0; i < 5; i++){
                     var base = responseData.daily[i]
-                    console.log(base.wind_speed)
-                    console.log(base.humidity)
-                    console.log(base.uvi)
-                    console.log(base.temp.day)
                     var timestamp = base.dt
                     var date = new Date(timestamp*1000)
-                    console.log(date)
+                    total = document.createElement('span') 
+                    total.classList.add('dayToo');                
+                    total.textContent =  date + '\nTemp: ' + base.temp.day + '\nWind: ' + base.wind_speed + '\nHumidity: ' + base.humidity + '\nUvi: ' + base.uvi 
+                    // total.textContent = base.wind_speed
+                    // total.textContent = base.humidity
+                    // total.textContent = base.uvi
+                    // total.textContent = date
+                    // total.textContent = base.icon
+                    existDiv.appendChild(total)
+                    // existDiv.textContent = "</span>"
+                    
+                   
                 }
               
-
+                
 
             });
         }
 
-      console.log(apiCall())
+
+
+
+        apiCall();
+
+
+        // console.log(userInput)
+    //   console.log(apiCall())
+
+  
+// console.log(nameValue)
+// fetchButton.addEventListener('click', console.log(nameValue));
+
+// console.log(apiCall2(nameValue))
+
+
